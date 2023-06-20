401771d556432c074a35fdff62d6d6decb301180
martin <realbobrossthepainter@gmail.com>
2023-06-20 01:45:27 -0700

    Added a test class: HardwareQueue * Finish tmrw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/HardwareQueue.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import java.util.Hashtable;
+
+interface Functor {
+    Object func(Object... args);
+}
+
+enum HardwareBus {
+    I2C,
+    MOTOR,
+    ANALOG,
+    DIGITAL,
+    SERVO
+}
+
+public class HardwareQueue {
+    Hashtable<Functor, Object[]>[] functors = new Hashtable[HardwareBus.values().length];
+
+    public HardwareQueue() {
+        for (int i = 0; i < HardwareBus.values().length; i++) {
+            functors[i] = new Hashtable<>();
+        }
+    }
+
+    public void addRequest(Functor functor, HardwareBus bus, Object... args) {
+        // The least jank thing about this entire class
+        int i = 0;
+        for (HardwareBus b : HardwareBus.values()) {
+            if (b == bus) {
+                break;
+            }
+            i++;
+        }
+
+        functors[i].put(functor, args);
+    }
+
+    public void update() {
+        // TODO tmrw
+    }
+}
