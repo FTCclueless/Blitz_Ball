a7ab3cf19acbe227063f0f60faa0413b4d80c302
Kynnet <110259702+Kynnet@users.noreply.github.com>
2023-07-24 21:34:20 -0700

    imu change localizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/Aim/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/Aim/Shooter.java
@@ -3,14 +3,18 @@ package org.firstinspires.ftc.teamcode.subsystems.drive.Aim;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.utils.MotorPriority;
+import org.firstinspires.ftc.teamcode.utils.MyServo;
 import org.firstinspires.ftc.teamcode.utils.PID;
 
 import java.util.ArrayList;
 
 public class Shooter {
     DcMotorEx shooter;
+    MyServo leftShooterHood;
+    MyServo rightShooterHood;
 
 
     ArrayList<MotorPriority> motorPriorities;
@@ -29,6 +33,9 @@ public class Shooter {
 
     public Shooter(HardwareMap hardwareMap, ArrayList<MotorPriority> motorPriorities) {
         shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        leftShooterHood = new MyServo(hardwareMap.get(Servo.class, "leftShooterHood"),"axon");
+        rightShooterHood = new MyServo(hardwareMap.get(Servo.class, "rightShooterHood"), "axon");
+
         this.motorPriorities = motorPriorities;
 
         motorPriorities.add(new MotorPriority(shooter, 3, 5));
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MyServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MyServo.java
@@ -3,15 +3,18 @@ package org.firstinspires.ftc.teamcode.utils;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class MyServo {
-    double axonPositionPerAngle;
+    double axonPositionPerAngle = 0.2; //temp angle
+    double axonZero = 0;
+    String type;
     Servo servo;
-    public MyServo(Servo servo) {
+    public MyServo(Servo servo, String type) {
         this.servo = servo;
+        this.type = type;
     }
 
-    public void setAngle(double angle, String type) {
+    public void setAngle(double angle) {
         if (type == "axon") {
-            servo.setPosition(angle * axonPositionPerAngle);
+            servo.setPosition(angle * axonPositionPerAngle + axonZero);
         }
     }
 }
