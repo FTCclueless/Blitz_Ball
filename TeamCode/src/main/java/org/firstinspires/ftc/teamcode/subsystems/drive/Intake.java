b8bb2bcf9aad0dbe1291d899e86bbc52d25dde9d
Patriot734 <108592219+Patriot734@users.noreply.github.com>
2023-07-27 17:56:05 -0700

    Finished intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/Intake.java
@@ -3,15 +3,32 @@ package org.firstinspires.ftc.teamcode.subsystems.drive;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.utils.MotorPriority;
+
 import java.util.ArrayList;
 
 public class Intake {
     DcMotorEx intake;
     ArrayList<DcMotorEx> motorPriorities;
 
+    boolean intakeState = true;
+
+    double maxSpeed = 1.0;
 
     public Intake(HardwareMap hardwareMap, ArrayList<DcMotorEx> motorPriorities) {
-        hardwareMap.get(DcMotorEx.class, "intake");
         this.motorPriorities = motorPriorities;
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+
+        motorPriorities.add((DcMotorEx) new MotorPriority (intake, 3, 5));
+
     }
+
+    public void turnOnIntake() {
+        intake.setPower(maxSpeed);
+    }
+    public void turnOffIntake()
+    {
+        intake.setPower(0);
+    }
+
 }
