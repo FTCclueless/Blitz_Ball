e928bbf23113f426342ae0320a656f2d58713702
Kynnet <110259702+Kynnet@users.noreply.github.com>
2023-07-28 17:00:19 -0700

    Turret Test + organization fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/TurretTest.java
@@ -6,17 +6,22 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.subsystems.drive.aim.Turret;
 import org.firstinspires.ftc.teamcode.subsystems.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.drive.aim.TurretState;
 import org.firstinspires.ftc.teamcode.utils.TelemetryUtil;
+
 
 @Autonomous(group = "tests")
 public class TurretTest extends LinearOpMode {
     double x = 0;
+    public static double target;
+
+
     @Override
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(hardwareMap);
         Drivetrain drivetrain = robot.drivetrain;
         Turret turret =  robot.turret;
-
+        turret.turretState = TurretState.AUTOAIM;
         Turret.pidEnabled = false;
 
         waitForStart();
@@ -24,9 +29,12 @@ public class TurretTest extends LinearOpMode {
         while(!isStopRequested()) {
             // drivetrain.drive(gamepad1);
 
-            drivetrain.update();
-            turret.update();
+
+            TelemetryUtil.packet.put("error", turret.errorAngle);
+            TelemetryUtil.packet.put("current", turret.currentAngle);
+            TelemetryUtil.packet.put("target", target);
             robot.update();
+            turret.setTargetAngle(target);
 
 
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/aim/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/aim/Turret.java
@@ -10,18 +10,11 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.sensors.Sensors;
-import org.firstinspires.ftc.teamcode.utils.AngleUtil;
 import org.firstinspires.ftc.teamcode.utils.MotorPriority;
 import org.firstinspires.ftc.teamcode.utils.PID;
 import org.firstinspires.ftc.teamcode.utils.TrapezoidMotionProfile;
 
 import java.util.ArrayList;
-
-enum State {
-    PID_ENABLED,
-    AUTOAIM,
-    MANUAL_CONTROL
-}
 
 @Config
 public class Turret {
@@ -30,7 +23,7 @@ public class Turret {
     public static final double maxRotation = Math.toRadians(270);
     public static double maxVelocity = 2540;
     public static double maxAccel = 2000;
-    public State state = State.MANUAL_CONTROL;
+    public TurretState turretState = TurretState.MANUAL_CONTROL;
     public static boolean pidEnabled = false;
     public static PID turretPid = new PID(1,0,0);
 
@@ -43,20 +36,22 @@ public class Turret {
     public double zeroAngle;
 
     DcMotorEx turretMotor;
-    double errorAngle;
+    public double errorAngle;
 
     double turretVelocity;
     double turretPower;
     ArrayList<MotorPriority> motorPriorities;
 
     public static TrapezoidMotionProfile turretMotionProfile = new TrapezoidMotionProfile(maxAccel,maxVelocity);
+    public static double errorMargin;
 
 
 
-    public Turret(HardwareMap hardwareMap, ArrayList<MotorPriority> motorPriorities) {
+    public Turret(HardwareMap hardwareMap, ArrayList<MotorPriority> motorPriorities, Sensors sensors) {
         turretMotor = hardwareMap.get(DcMotorEx.class, "turret");
         turretMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.sensors = sensors;
 
 
         zeroAngle = sensors.getTurretAngle() / ticksPerRadian;
@@ -95,13 +90,16 @@ public class Turret {
         errorAngle = targetAngle - currentAngle;
         turretVelocity = sensors.getTurretVelocity() / ticksPerRadian;
 
-        switch (state) {
+        switch (turretState) {
             case MANUAL_CONTROL:
                 // FIXME: ask later if we should pass gamepad to all updates
                 break;
             case AUTOAIM:
                 turretPower = turretMotionProfile.getTargetVel(currentAngle);
                 motorPriorities.get(4).setTargetPower(turretPower);
+                /*if (isComplete(errorMargin)) {
+                    turretState = TurretState.AUTOAIM;
+                }*/
                 break;
             case PID_ENABLED:
                 turretPower = turretPid.getOut(errorAngle);
